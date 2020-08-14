d49395c6cae3533067d924155d80b398b9c59dbe
Tobias Graski <57156093+WahlMandat@users.noreply.github.com>
2020-08-14 23:11:16 +0200

    Add util functions

---
+++ utils.js
@@ -0,0 +1,7 @@
+let config = require('./datastore/configs.json');
+function serverConfiguration(guildId) {
+    return config.find(element => Number(guildId) == element.guildId);
+   
+}
+
+module.exports.serverConfiguration = serverConfiguration;
