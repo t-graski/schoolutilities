f4ea6192ae78ee16fb1587676cbdef91c2e2f986
Tobias Graski <57156093+WahlMandat@users.noreply.github.com>
2020-08-14 23:09:21 +0200

    Help additions

--- commands/config.js
+++ commands/config.js
@@ -2,10 +2,12 @@ const discord = require('discord.js');
 const fs = require('fs');
 const datastorePath = './datastore/configs.json';
 let configData;
+let isFirstConfig = false;
 try {
     configData = require('../datastore/configs.json');
 } catch (error) {
     configData = [];
+    isFirstConfig = true;
 }
 
 exports.run = async (client, message, args) => {
@@ -14,10 +16,10 @@ exports.run = async (client, message, args) => {
             let serverArrayId
             try {
                 // Refactor <-- works, but it's ugly
-                if(!configData.find((configData, index) => {
+                if (!configData.find((configData, index) => {
                     serverArrayId = index;
                     return configData.guildId == message.guild.id;
-                })){
+                })) {
                     configData.push({
                         guildId: message.guild.id,
                         name: message.guild.name,
@@ -26,29 +28,43 @@ exports.run = async (client, message, args) => {
                         timeZone: "GMT+0",
                         language: "english",
                         channel: "bot-config",
-                        checktime: 3000
+                        checktime: 3
                     });
                     serverArrayId = configData.length - 1;
                 }
             } catch (error) {
                 console.log(error);
             }
-            if (args[0] == "student") {
-                configData[serverArrayId].studentId = Number(message.guild.roles.cache.find(role => role.name === args[1]).id);
-                console.table(configData)
-            } else if (args[0] == "teacher") {
-                configData[serverArrayId].teacherId = Number(message.guild.roles.cache.find(role => role.name === args[1]).id);
-                console.table(configData)
-            } else if (args[0] == "timezone") {
-                configData[serverArrayId].timeZone = args[1];
-                console.table(configData)
-            } else if (args[0] == "language") {
-                configData[serverArrayId].language = args[1];
-                console.table(configData)
-            } else if (args[0] == "checktime") {
-                configData[serverArrayId].checktime = args[1];
-                console.table(configData)
+            let replyMsg = "";
+            if(isFirstConfig){
+                replyMsg = "Hi, nice to have you in the schoolutilities community. That's your first configuration, have fun with this bot!\n";
+                isFirstConfig = false;
             }
+            let configInput = args[0].toLowerCase();
+            if (configInput == "student") {
+                configData[serverArrayId].studentId = message.guild.roles.cache.find(role => role.name.toLowerCase() === args[1].toLowerCase()).id;
+                console.table(configData);
+                replyMsg += `Your student role has been updated.`;
+            } else if (configInput == "teacher") {
+                configData[serverArrayId].teacherId = message.guild.roles.cache.find(role => role.name.toLowerCase() === args[1].toLowerCase()).id;
+                console.table(configData);
+                replyMsg += `Your teacher role has been updated.`;
+            } else if (configInput == "timezone") {
+                configData[serverArrayId].timeZone = args[1].toLowerCase();
+                console.table(configData);
+                replyMsg += `Your timezone has been updated.`;
+            } else if (configInput == "language") {
+                configData[serverArrayId].language = args[1].toLowerCase();
+                console.table(configData);
+                replyMsg += `Your language has been updated.`;
+            } else if (configInput == "checktime") {
+                configData[serverArrayId].checktime = args[1];
+                console.table(configData);
+                replyMsg += `Your checktime has been updated.`;
+            } else {
+                replyMsg += `There was a problem updating your config, please check, if you've written it correctly.`
+            }
+            message.channel.send(replyMsg);
             save(datastorePath, JSON.stringify(configData, null, '\t'));
         }
     }
