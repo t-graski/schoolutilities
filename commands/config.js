4914aa150fa628a2b4618fc1a7eec43b0d79cbb8
dav-prog <david.woegerb@gmail.com>
2020-08-15 21:12:33 +0200

    Added timetable

--- commands/config.js
+++ commands/config.js
@@ -1,6 +1,10 @@
 const discord = require('discord.js');
 const fs = require('fs');
+const { time } = require('console');
 const datastorePath = './datastore/configs.json';
+const translate = require('google-translate-free');
+const { string, sort } = require('mathjs');
+const numeral = require('numeral');
 let configData;
 let isFirstConfig = false;
 try {
@@ -28,7 +32,17 @@ exports.run = async (client, message, args) => {
                         timeZone: "GMT+0",
                         language: "english",
                         channel: "bot-config",
-                        checktime: 3
+                        checktime: 3,
+                        autocheck: false,
+                        timeTable: {
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                            5: [],
+                            6: [],
+                            7: []
+                        }
                     });
                     serverArrayId = configData.length - 1;
                 }
@@ -36,17 +50,19 @@ exports.run = async (client, message, args) => {
                 console.log(error);
             }
             let replyMsg = "";
-            if(isFirstConfig){
+            if (isFirstConfig) {
                 replyMsg = "Hi, nice to have you in the schoolutilities community. That's your first configuration, have fun with this bot!\n";
                 isFirstConfig = false;
             }
             let configInput = args[0].toLowerCase();
             if (configInput == "student") {
-                configData[serverArrayId].studentId = message.guild.roles.cache.find(role => role.name.toLowerCase() === args[1].toLowerCase()).id;
+                let inputRole = args.slice(1).reduce((a, b) => a + " " + b)
+                configData[serverArrayId].studentId = message.guild.roles.cache.find(role => role.name.toLowerCase() === inputRole.toLowerCase()).id;
                 console.table(configData);
                 replyMsg += `Your student role has been updated.`;
             } else if (configInput == "teacher") {
-                configData[serverArrayId].teacherId = message.guild.roles.cache.find(role => role.name.toLowerCase() === args[1].toLowerCase()).id;
+                let inputRole = args.slice(1).reduce((a, b) => a + " " + b)
+                configData[serverArrayId].teacherId = message.guild.roles.cache.find(role => role.name.toLowerCase() === inputRole.toLowerCase()).id;
                 console.table(configData);
                 replyMsg += `Your teacher role has been updated.`;
             } else if (configInput == "timezone") {
@@ -61,6 +77,90 @@ exports.run = async (client, message, args) => {
                 configData[serverArrayId].checktime = args[1];
                 console.table(configData);
                 replyMsg += `Your checktime has been updated.`;
+            } else if (configInput == "autocheck") {
+                if (args[1].toLowerCase() == "enable") {
+                    configData[serverArrayId].autocheck = true;
+                } else if (args[1].toLowerCase() == "disable") {
+                    configData[serverArrayId].checktime = false;
+                }
+                console.table(configData);
+                replyMsg += `Your autocheck has been updated.`;
+            } else if (configInput == "timetable") {
+                if (args[1].toLowerCase() == "add") {
+                    //Check the weekday for the timetable entry
+                    let weekday = getWeekDay(args[2]);
+
+                    //Check, if time is correct
+                    let subject = args.length > 0 ? args.slice(6).reduce((a, b) => a + " " + b) : args[6];
+                    let dateEntry = convertTimeEntryToObject(args[3], args[4], args[5], subject);
+                    if (dateEntry != false) {
+                        configData[serverArrayId].timeTable[weekday].push(dateEntry);
+                        console.table(configData[serverArrayId].timeTable);
+                        replyMsg += "Edited timetable";
+                        if (isTimeOverlaped(dateEntry, configData[serverArrayId].timeTable[weekday])) {
+                            replyMsg += "\n**Warning**: Your time entry overlapes with another one, if you want to remove the entry, please use remove. (For more informations type .help)";
+                        }
+                    } else {
+                        console.log("Something went wrong!");
+                        replyMsg += "The edit went wrong";
+                    }
+                } else if (args[1].toLowerCase() == "remove") {
+                    //Check the weekday for the timetable entry
+                    let weekday = getWeekDay(args[2]);
+
+                    //Check, if time is correct
+                    let subject = args.slice(6).reduce((a, b) => a + " " + b);
+                    let dateEntry = convertTimeEntryToObject(args[3], args[4], args[5], subject);
+                    let isDeleted = deleteTimeEntry(dateEntry, weekday);
+                    if (isDeleted) {
+                        replyMsg += `The subject has been deleted!`;
+                    } else {
+                        console.log("Something went wrong!");
+                        replyMsg += "The delete went wrong";
+                    }
+
+
+                    function deleteTimeEntry(dateEntry, weekday) {
+                        let deleteIndex = false;
+                        configData[serverArrayId].timeTable[weekday].find((timeEntry, index) => {
+                            if (isSameEntry(timeEntry, dateEntry)) {
+                                deleteIndex = index;
+                            }
+                        });
+
+                        if (deleteIndex != false) {
+                            configData[serverArrayId].timeTable[weekday].splice(deleteIndex, 1);
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                } else if (args[1].toLowerCase() == "clear") {
+                    if (args[2]) {
+                        let weekday = getWeekDay(args[2]);
+                        if (weekday) {
+                            configData[serverArrayId].timeTable[weekday] = [];
+                            replyMsg += `The day has been wiped.`
+                        } else {
+                            replyMsg += `Please enter a valid weekday.`
+                        }
+                    } else {
+                        configData[serverArrayId].timeTable = {
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                            5: [],
+                            6: [],
+                            7: []
+                        };
+                        replyMsg += `The entire timetable has been wiped.`
+                    }
+                } else if (args[1].toLowerCase() == "print") {
+                    replyMsg += getTimeTableString(configData[serverArrayId].timeTable);
+                } else {
+                    console.log("Wrong input:" + args[1]);
+                }
             } else {
                 replyMsg += `There was a problem updating your config, please check, if you've written it correctly.`
             }
@@ -71,4 +171,158 @@ exports.run = async (client, message, args) => {
 }
 function save(path, string) {
     fs.writeFile(path, string, (err) => { console.log(err ? err : ''); });
+}
+
+function checkTime(timeString) {
+    let hours = timeString.split(":")[0];
+    let minutes = timeString.split(":")[1];
+
+    if (hours < 24 && hours >= 0 && minutes < 60 && minutes >= 0) {
+        let date = {
+            hours: Number(hours),
+            minutes: Number(minutes)
+        }
+        return date;
+    } else {
+        return false;
+    }
+}
+
+function checkTimesMatching(startTime, endTime) {
+    return (startTime.hours == endTime.hours && startTime.minutes < endTime.minutes) || (startTime.hours < endTime.hours);
+}
+
+function convertTimeEntryToObject(startTime, endTime, channel, subjectName) {
+    channel = channel.replace(/\D/g, "");
+    startTime = checkTime(startTime);
+    endTime = checkTime(endTime);
+
+    if (startTime != false && endTime != false && checkTimesMatching(startTime, endTime) && subjectName.length > 1 && subjectName.length < 30) {
+        return {
+            "startTime": startTime,
+            "endTime": endTime,
+            "subject": subjectName,
+            "channel": channel
+        };
+    } else {
+        return false;
+    }
+}
+
+function isTimeOverlaped(timeObject, dayTimeTable) {
+    let isTimeOverlaped = false;
+    let startTime = (timeObject.startTime.hours * 60 + timeObject.startTime.minutes) * 60;
+    let endTime = (timeObject.endTime.hours * 60 + timeObject.endTime.minutes) * 60;
+
+    dayTimeTable.forEach(timeEntry => {
+        let entryStartTime = (timeEntry.startTime.hours * 60 + timeEntry.startTime.minutes) * 60;
+        let entryEndTime = (timeEntry.endTime.hours * 60 + timeEntry.endTime.minutes) * 60;
+
+        if ((startTime > entryStartTime && startTime < entryEndTime) || (endTime > entryStartTime && endTime < entryEndTime)) {
+            isTimeOverlaped = true;
+        }
+    });
+    return isTimeOverlaped;
+}
+
+function getWeekDay(dayArg) {
+    dayArg = dayArg.toLowerCase();
+    // dayArg = translateWeekDay(dayArg);
+    let weekday;
+    if (dayArg == "mo" || dayArg == "monday") {
+        weekday = 1;
+    } else if (dayArg == "tu" || dayArg == "tuesday") {
+        weekday = 2;
+    } else if (dayArg == "we" || dayArg == "wednesday") {
+        weekday = 3;
+    } else if (dayArg == "th" || dayArg == "thursday") {
+        weekday = 4;
+    } else if (dayArg == "fr" || dayArg == "friday") {
+        weekday = 5;
+    } else if (dayArg == "sa" || dayArg == "saturday") {
+        weekday = 6;
+    } else if (dayArg == "su" || dayArg == "sunday") {
+        weekday = 7;
+    } else {
+        weekday = false;
+    }
+    return weekday;
+}
+
+// Fix this later
+
+// function translateWeekDay(weekDay) {
+//     translate(weekDay, { to: 'en' }).then(res => {
+//         return res.text;
+//     }).catch(err => {
+//         return false;
+//     });
+// }
+
+function isSameEntry(entryOne, entryTwo) {
+    return (entryOne.startTime.hours == entryTwo.startTime.hours && entryOne.startTime.minutes == entryTwo.startTime.minutes && entryOne.endTime.hours == entryTwo.endTime.hours && entryOne.endTime.minutes == entryTwo.endTime.minutes && entryOne.subject == entryTwo.subject);
+}
+
+function getTimeTableString(timeTable) {
+    let stringToPrint = "";
+
+    let weekdays = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
+
+    weekdays.forEach((currentDay, index) => {
+        let sortedTimeTable = timeTable[index + 1].slice();
+        let weekDay = getLongWeekDay();
+        if (sortedTimeTable.length != 0) {
+            stringToPrint += `**${currentDay}** :\n`;
+            sortedTimeTable = sortedTimeTable.sort((a, b) => checkTimesMatching(a.startTime, b.startTime) ? -1 : 1);
+            sortedTimeTable.forEach(element => {
+                if (weekDay == currentDay) {
+                    if (timeInRange(element.startTime, element.endTime)) {
+                        stringToPrint += `__**${numeral(element.startTime.hours).format("00")}:${numeral(element.startTime.minutes).format("00")} - ${numeral(element.endTime.hours).format("00")}:${numeral(element.endTime.minutes).format("00")}: ${element.subject}**__\n`;
+                    }
+                } else {
+
+                    stringToPrint += `${numeral(element.startTime.hours).format("00")}:${numeral(element.startTime.minutes).format("00")} - ${numeral(element.endTime.hours).format("00")}:${numeral(element.endTime.minutes).format("00")}: ${element.subject}\n`;
+                }
+            });
+            stringToPrint += "\n";
+        }
+    });
+
+    return stringToPrint;
+}
+function getLongWeekDay() {
+    let d = new Date();
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    return weekday[d.getDay()];
+}
+function timeInRange(startTime, endTime) {
+    let startTimeSec = (startTime.hours * 60 + startTime.minutes) * 60;
+    let endTimeSec = (endTime.hours * 60 + endTime.minutes) * 60;
+
+    let date = new Date();
+    let currentHour = date.getHours();
+    let currentMinute = date.getMinutes();
+    let currentTimeSec = (currentHour * 60 + currentMinute) * 60;
+
+    if (currentTimeSec > startTimeSec && currentTimeSec < endTimeSec)
+        return true;
+    else
+        return false;
+
 }
