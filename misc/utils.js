884dbe0b0ad477b79f852c9100b23bec1a19be88
Tobias Graski <57156093+WahlMandat@users.noreply.github.com>
2020-11-14 22:22:06 +0100

    Add save function

--- misc/utils.js
+++ misc/utils.js
@@ -2,5 +2,11 @@ let config = require('../datastore/configs.json');
 function serverConfiguration(guildId) {
     return config.find((element) => Number(guildId) == element.guildId);
 }
+function save(path, string) {
+    fs.writeFile(path, string, (err) => {
+        console.log(err ? err : '');
+    });
+}
 
 module.exports.serverConfiguration = serverConfiguration;
+module.exports.save = save;
