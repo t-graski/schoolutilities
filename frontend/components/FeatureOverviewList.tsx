b0706794907f73f93af109dfe6b48efe8ea2f526
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2021-09-13 21:22:18 +0200

    Add FeatureOverview and FeatureOverviewList component

---
+++ frontend/components/FeatureOverviewList.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { styled } from "../stitches.config";
+import { FeatureOverview, FeatureOverviewProps } from "./FeatureOverview";
+import { Seperator } from "./Seperator";
+
+type Props = {
+  features: FeatureOverviewProps[];
+};
+
+const FeatureOverviewListLayout = styled("div", {
+  width: "100%",
+  padding: "0 15vw",
+});
+
+export const FeatureOverviewList: React.FC<Props> = ({ features }) => {
+  return (
+    <>
+      <FeatureOverviewListLayout>
+        {features.map((feature, index) => (
+            <>
+          <FeatureOverview
+            imageSrc={feature.imageSrc}
+            imageAlt={feature.imageAlt}
+            title={feature.title}
+            description={feature.description}
+            imagePosition={
+              feature.imagePosition
+                ? feature.imagePosition
+                : index % 2 == 0
+                ? "left"
+                : "right"
+            }
+          />
+          {index < features.length - 1 && <Seperator width="big" alignment="center"/>}
+            </>
+        ))}
+      </FeatureOverviewListLayout>
+    </>
+  );
+};
--- frontend/pages/features.tsx
+++ frontend/pages/features.tsx
@@ -7,6 +7,8 @@ import { Headline } from "../components/Headline";
 import React from "react";
 import { Spacer } from "../components/Spacer";
 import { Seperator } from "../components/Seperator";
+import { FeatureOverview } from "../components/FeatureOverview";
+import { FeatureOverviewList } from "../components/FeatureOverviewList";
 
 export default function Features() {
   return (
@@ -29,7 +31,39 @@ export default function Features() {
       ></Navbar>
       <Spacer size="medium"></Spacer>
       <Headline label="Features"></Headline>
-      <Seperator width="small" alignment="center"/>
+      <Seperator width="small" alignment="center" />
+      <FeatureOverviewList
+        features={[
+          {
+            imageSrc: "/images/timetable.svg",
+            imageAlt: "Timetable icon",
+            title: "#1 - Timetable",
+            description:
+              "Construct your timetable depending on your personal needs. Easily set up your timetable with just a few simple steps on our Dashboard.",
+          },
+          {
+            imageSrc: "/images/calculator.svg",
+            imageAlt: "Timetable icon",
+            title: "#2 - Calculation",
+            description:
+              "Feel the need to quickly solve some math problems? Just use our new calculation feature!",
+          },
+          {
+            imageSrc: "/images/immigration.svg",
+            imageAlt: "Attendance icon",
+            title: "#3 - Attendance",
+            description:
+              "Easier than ever before to check the attendance of your students or friends. This feature also comes with customizable time and roles to check.",
+          },
+          {
+            imageSrc: "/images/promotion.svg",
+            imageAlt: "Alert icon",
+            title: "#4 - Alert",
+            description:
+              "You are a teacher, and want to send a message to all your students? This is now easily possible through the use of our alert feature.",
+          },
+        ]}
+      ></FeatureOverviewList>
       <Footer
         links={[
           {
