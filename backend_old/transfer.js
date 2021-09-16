96e1fa96b27c0edb4976a526044911780098ae26
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2021-09-16 21:29:15 +0200

    Fix transfer.js for new database structure

--- backend_old/transfer.js
+++ backend_old/transfer.js
@@ -56,60 +56,78 @@ try {
 //     "prefix": "."
 // }
 
-async function test (){
-for (let i = 0; i < configData.length; i++) {
-    const config = configData[i];
-    let server = await addServer({
-        guild_id: config.guildId,
-        name: config.name,
-        language: config.language,
-        timezone: config.timeZone,
-    });
-    let serverId = server.insertId;
-    let schoolclass = await addClass({
-        server_id: serverId,
-        student_id: config.studentId,
-        teacher_id: config.teacherId,
-        checktime: config.checktime,
-        autocheck: config.autocheck,
-        notifications: config.notifications,
-    });
-    let classId = schoolclass.insertId;
-    for (let weekday in config.timeTable) {
-        let timeTable = config.timeTable[weekday];
-        for (let time of timeTable) {
-            await addTimetableEntry({
-                class_id: classId,
-                weekday: weekday,
-                start_time: `${time.startTime.hours}:${time.startTime.minutes}`,
-                end_time: `${time.endTime.hours}:${time.endTime.minutes}`,
-                channel: time.channel,
-                subject_name: time.subject,
-            });
+async function test() {
+    for (let i = 0; i < configData.length; i++) {
+        const config = configData[i];
+        let school = await addSchool({
+            guild_id: config.guildId,
+            name: config.name,
+            language: config.language,
+            timezone: config.timeZone,
+        });
+        let schoolId = school.insertId;
+        await addDiscordServer({
+            school_id: schoolId,
+            guild_id: config.guildId,
+            server_name: config.name,
+        });
+        let schoolclass = await addClass({
+            server_id: schoolId,
+            student_id: config.studentId,
+            teacher_id: config.teacherId,
+            checktime: config.checktime,
+            autocheck: config.autocheck,
+            notifications: config.notifications,
+        });
+        let classId = schoolclass.insertId;
+        for (let weekday in config.timeTable) {
+            let timeTable = config.timeTable[weekday];
+            for (let timeTableEntryJson of timeTable) {
+                let timeTableEntry = await addTimetableEntry({
+                    class_id: classId,
+                    weekday: weekday,
+                    start_time: `${timeTableEntryJson.startTime.hours}:${timeTableEntryJson.startTime.minutes}`,
+                    end_time: `${timeTableEntryJson.endTime.hours}:${timeTableEntryJson.endTime.minutes}`,
+                    channel: timeTableEntryJson.channel,
+                    subject_name: timeTableEntryJson.subject,
+                });
+                let timeTableEntryId = timeTableEntry.insertId;
+                await addDiscordTimetableEntry({
+                    timetable_entry_id: timeTableEntryId,
+                    channel_id: timeTableEntryJson.channel,
+                });
+            }
         }
+        // Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 10);
+        console.log(`${i + 1}/${configData.length} (${Math.round(((i + 1) / configData.length) * 10000) / 100}%)`);
     }
-    // Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 10);
-    console.log(`${i+1}/${configData.length} (${Math.round((i+1)/configData.length*10000)/100}%)`);
-}
 }
 test();
 
 async function test2() {
-    let returnValue = await getSubjectIdOfClassId(5, "Mathematik");
+    let returnValue = await getSubjectIdOfClassId(5, 'Mathematik');
     console.log(returnValue);
 }
 // test2();
 
-async function addServer(server) {
+async function addSchool(server) {
     return new Promise((resolve, reject) => {
         connection.query(
-            'INSERT INTO `server` (guild_id, name, language, timezone) VALUES(?, ?, ?, ?)',
-            [
-                server.guild_id,
-                server.name,
-                server.language,
-                server.timezone,
-            ],
+            'INSERT INTO `school` (name, language, timezone) VALUES(?, ?, ?)',
+            [server.name, server.language, server.timezone],
+            //@ts-ignore
+            function (error, results, fields) {
+                resolve(results);
+            }
+        );
+    });
+}
+
+async function addDiscordServer(server) {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            'INSERT INTO `discord_server` (school_id, guild_id, server_name) VALUES(?, ?, ?)',
+            [server.school_id, server.guild_id, server.server_name],
             //@ts-ignore
             function (error, results, fields) {
                 resolve(results);
@@ -120,12 +138,12 @@ async function addServer(server) {
 
 async function addClass(schoolclass) {
     let checkTime = parseInt(schoolclass.checktime);
-    if(Number.isNaN(checkTime) || checkTime + "".includes("<") || checkTime + "".includes(">")) {
+    if (Number.isNaN(checkTime) || checkTime + ''.includes('<') || checkTime + ''.includes('>')) {
         checkTime = 1;
     }
     return new Promise((resolve, reject) => {
         connection.query(
-            'INSERT INTO `class` (server_id, student_id, teacher_id, checktime, autocheck, notifications) VALUES(?, ?, ?, ?, ?, ?)',
+            'INSERT INTO `class` (school_id, student_id, teacher_id, checktime, autocheck, notifications) VALUES(?, ?, ?, ?, ?, ?)',
             [
                 schoolclass.server_id,
                 schoolclass.student_id,
@@ -145,23 +163,16 @@ async function addClass(schoolclass) {
 async function addTimetableEntry(entry) {
     let subject_id;
     let subjectInDatabase = await getSubjectIdOfClassId(entry.class_id, entry.subject_name);
-    if(subjectInDatabase && subjectInDatabase.length === 0) {
-        let subject = await addSubject({name: entry.subject_name, class_id: entry.class_id});
+    if (subjectInDatabase && subjectInDatabase.length === 0) {
+        let subject = await addSubject({ name: entry.subject_name, class_id: entry.class_id });
         subject_id = subject.insertId;
     } else {
         subject_id = subjectInDatabase[0].subject_id;
     }
     return new Promise((resolve, reject) => {
         connection.query(
-            'INSERT INTO `timetable_entry` (weekday, starttime, endtime, channel_id, class_id, subject_id) VALUES(?, ?, ?, ?, ?, ?)',
-            [
-                entry.weekday,
-                entry.start_time,
-                entry.end_time,
-                entry.channel,
-                entry.class_id,
-                subject_id,
-            ],
+            'INSERT INTO `timetable_entry` (weekday, starttime, endtime, class_id, subject_id) VALUES(?, ?, ?, ?, ?)',
+            [entry.weekday, entry.start_time, entry.end_time, entry.class_id, subject_id],
             //@ts-ignore
             function (error, results, fields) {
                 resolve(results);
@@ -174,7 +185,7 @@ async function getSubjectIdOfClassId(class_id, subject_name) {
     let subjectTrimmed = subject_name.trim();
     return new Promise((resolve, reject) => {
         connection.query(
-            "SELECT * FROM `subject` WHERE class_id=? AND subject_name like ?",
+            'SELECT * FROM `subject` WHERE class_id=? AND subject_name like ?',
             [class_id, subjectTrimmed],
             //@ts-ignore
             function (error, results, fields) {
@@ -184,15 +195,25 @@ async function getSubjectIdOfClassId(class_id, subject_name) {
     });
 }
 
-async function addSubject(subject){
+async function addDiscordTimetableEntry(timetableEntryData) {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            'INSERT INTO `discord_timetable_entry` (timetable_entry_id, channel_id) VALUES(?, ?)',
+            [timetableEntryData.timetable_entry_id, timetableEntryData.channel_id],
+            //@ts-ignore
+            function (error, results, fields) {
+                resolve(results);
+            }
+        );
+    });
+}
+
+async function addSubject(subject) {
     return new Promise((resolve, reject) => {
         let subjectTrimmed = subject.name.trim();
         connection.query(
             'INSERT INTO `subject` (subject_name, class_id) VALUES(?, ?)',
-            [
-                subjectTrimmed,
-                subject.class_id,
-            ],
+            [subjectTrimmed, subject.class_id],
             //@ts-ignore
             function (error, results, fields) {
                 resolve(results);
