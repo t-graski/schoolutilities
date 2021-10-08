d05e5d67fd6ecba0e3cc6c680270f210d4d9d192
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2021-10-09 00:02:53 +0200

    Add departments endpoint

--- backend/src/types/SchoolAdmin.ts
+++ backend/src/types/SchoolAdmin.ts
@@ -1,13 +1,40 @@
-export interface AddSchoolConfig {
+export interface AddSchool {
   name: string;
   languageId: number;
   timezone: string;
 }
 
-export interface AddSchoolConfigReturnValue {
+export interface AddSchoolReturnValue {
   status: number;
   message: string;
   data?: {
     schoolId: number;
   };
+}
+
+export interface AddDepartment {
+  name: string;
+  schoolId: number;
+  isVisible?: string;
+  childsVisible?: boolean;
+}
+
+export interface AddDepartmentReturnValue {
+  status: number;
+  message: string;
+  data?: {
+    departmentId: number;
+  };
+}
+
+export interface ReturnMessage {
+  status: number;
+  message: string;
+}
+
+export interface UpdateDepartment {
+  name: string;
+  departmentId: number;
+  isVisible?: string;
+  childsVisible?: boolean;
 }
