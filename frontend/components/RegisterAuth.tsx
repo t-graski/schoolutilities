09b0e6965a16e30eb758dcabc742b50b55c5f368
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2021-11-14 21:58:15 +0100

    Fix backend link

--- frontend/components/RegisterAuth.tsx
+++ frontend/components/RegisterAuth.tsx
@@ -33,7 +33,7 @@ export const RegisterAuth = () => {
       token: token,
     });
     console.log(requestBody);
-    fetch("http://168.119.227.44:8888/api/user/activateAccount", {
+    fetch("https://168.119.227.44:8888/api/user/activateAccount", {
       method: "POST",
       body: requestBody,
       headers: { "Content-Type": "application/json" },
