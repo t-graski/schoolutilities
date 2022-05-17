dd24dda73479e53ad49b2f97e5d536b3f6da453f
Tobias Graski <tobias.graski@gmx.at>
2022-05-17 22:08:11 +0200

    Add logger

--- backend/src/applogger.middleware.ts
+++ backend/src/applogger.middleware.ts
@@ -1,14 +1,11 @@
-import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
-
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class RequestLogger implements NestMiddleware {
-    private logger = new Logger('HTTP');
-
     async use(request: Request, response: Response, next: NextFunction): Promise<void> {
         const startAt = process.hrtime();
         const { ip, method, originalUrl } = request;
@@ -16,17 +13,17 @@ export class RequestLogger implements NestMiddleware {
 
         response.on('finish', async () => {
             const { statusCode } = response;
-            const contentLength = response.get('content-length');
+            const contentLength = Number(response.get('content-length'));
             const diff = process.hrtime(startAt);
             const responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
 
             await prisma.requestLog.create({
                 data: {
-                    ip: ip,
+                    ip,
                     method,
                     path: originalUrl,
                     status: statusCode,
-                    contentLength: Number(contentLength),
+                    contentLength,
                     userAgent,
                     responseTime,
                 },
