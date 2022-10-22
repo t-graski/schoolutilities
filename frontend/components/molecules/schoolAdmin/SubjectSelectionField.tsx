2b5b6b0a1a01dea877cabdd3fc5512f98bb6ee72
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2022-10-22 12:50:12 +0200

    Work at timetable create component and fix ads bug

---
+++ frontend/components/molecules/schoolAdmin/SubjectSelectionField.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { styled } from "../../../stitches.config";
+import Image from "next/image";
+import Link from "next/link";
+import { useQuery } from "react-query";
+import { fetchSchoolSubjects } from "../../../utils/requests/admin";
+import { useRouter } from "next/router";
+import { SearchSelect } from "../../atoms/input/SearchSelect";
+
+type Props = {
+  selectedSubject: string;
+  setSelectedSubject: (subjectUUID: string) => void;
+};
+
+export const SubjectSelectionField: React.FC<Props> = ({
+  selectedSubject,
+  setSelectedSubject,
+}) => {
+  const router = useRouter();
+  const schoolUUID = router.query.schoolUUID as string;
+
+  const { data: subjects, status: subjectsStatus } = useQuery("subjects", () =>
+    fetchSchoolSubjects(schoolUUID)
+  );
+
+  if(subjectsStatus === "loading") return <div>Loading...</div>
+  
+  if(subjectsStatus === "error") return <div>Error</div>
+  
+  return <>
+    <SearchSelect
+        selectOptions={subjects?.map((subject) => ({
+            label: subject.schoolSubjectAbbreviation,
+            value: subject.schoolSubjectUUID,
+        }))}
+        selectValue={selectedSubject}
+        selectMultiValues={false}
+        onChange={(value) => setSelectedSubject(value.value)}
+        editable={true}
+        isSmall={true}
+    ></SearchSelect>
+  </>;
+};
---
+++ frontend/components/pages/schoolAdmin/EditTimetableItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { styled } from "../../../stitches.config";
+import Image from "next/image";
+import Link from "next/link";
+import { useQuery } from "react-query";
+import { fetchSchoolSubjects } from "../../../utils/requests/admin";
+import { useRouter } from "next/router";
+import { SearchSelect } from "../../atoms/input/SearchSelect";
+
+type Props = {
+};
+
+export const SubjectSelectionField: React.FC<Props> = ({}) => {
+  return <></>;
+};
