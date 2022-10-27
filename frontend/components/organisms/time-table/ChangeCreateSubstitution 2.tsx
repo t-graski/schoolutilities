89883083633e87a5e8d432cda149e77c7dcccde6
Meister Eder <57350244+EderEderEderEder@users.noreply.github.com>
2022-10-27 11:23:14 +0200

    add frontend

---
+++ frontend/components/organisms/time-table/ChangeCreateSubstitution 2.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { styled } from "../../../stitches.config";
+import { InputField } from "../../atoms/input/InputField";
+import { TimeTableItemType } from "../../atoms/TimeTableItem";
+
+export type Substitution = {
+    timeTableSubstitutionUUID?: string;
+    timeTableElementUUID: string;
+    timeTableTeachers: string[];
+    timeTableClasses: string[];
+}
+
+type Props = {
+    timeTableElement: TimeTableItemType;
+};
+
+const InputFieldsLayout = styled("div", {
+  display: "grid",
+  gridTemplateColumns: "2fr 1fr 1fr 1fr",
+});
+
+const InputFieldLayout = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+});
+
+export const ChangeCreateSubstitution: React.FC<Props> = ({}) => {
+  const [itemConfig, setItemConfig] = useState<any>({
+    timeTableElementStartTime: "",
+    timeTableElementEndTime: "",
+    schoolSubject: {
+      schoolSubjectUUID: "",
+      schoolSubjectName: "",
+      schoolSubjectAbbreviation: "",
+    },
+    teachers: [],
+    classes: [],
+  });
+
+  return (
+    <>
+      <InputFieldsLayout>
+        <InputFieldLayout>
+          <InputField
+            label="Name"
+            value={itemConfig.schoolSubjectName}
+            inputType={"text"}
+            onChange={(event) => {
+              setItemConfig({
+                ...itemConfig,
+                schoolSubjectName: event,
+              });
+            }}
+          ></InputField>
+        </InputFieldLayout>
+        <InputFieldLayout>
+          <InputField
+            label="Start Time"
+            value={itemConfig.startTime}
+            inputType={"time"}
+            onChange={(event) => {
+              setItemConfig({
+                ...itemConfig,
+                startTime: event,
+              });
+            }}
+          ></InputField>
+        </InputFieldLayout>
+        <InputFieldLayout>
+          <InputField
+            label="End Time"
+            value={itemConfig.endTime}
+            inputType={"time"}
+            onChange={(event) => {
+              setItemConfig({
+                ...itemConfig,
+                endTime: event,
+              });
+            }}
+          ></InputField>
+        </InputFieldLayout>
+      </InputFieldsLayout>
+    </>
+  );
+};
